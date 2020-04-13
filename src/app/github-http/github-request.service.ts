9d4309d4eb7ea66e888d710516c46a9bfb41d052
rmautia <raphaelnyangenya@gmail.com>
2020-04-13 17:04:02 +0300

    update profile component with data user information.

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { from } from 'rxjs';
+
+import { Observable } from 'rxjs-compat';
 
 @Injectable({
   providedIn: 'root'
@@ -11,33 +10,54 @@ import { from } from 'rxjs';
 export class GithubRequestService {
 
   private userName: string;
-  // private clientId: string = '<Client Id>';
-  // private clientSecret: string = '<Client Secret Key>';
-  private clientId: string = '922c8f6ccfa1661f8838da56c0a3d7208480ce60';
-  private clientSecret: string = 'd1c186c6373f96571c0bfcf76b84e4dc6fd0c15a';
+    private clientId: string;
+    private clientSecret: string;
+    private getUserUrl: string;
+    private getRepoUrl: string;
+    private getReadmeUrl: string;
 
   constructor(private _http: HttpClient) { 
-    //console.log('Github Service Ready.');
     this.userName = '';
+    this.clientId = '60b9f23dedffbdfc476c';
+    this.clientSecret = '922c8f6ccfa1661f8838da56c0a3d7208480ce60';
+    this.getUserUrl = 'https://api.github.com/users/[userName]?client_id=[clientId]&client_secret=[clientSecret]';
+    this.getRepoUrl = 'https://api.github.com/users/[userName]/repos?client_id=[clientId]&client_secret=[clientSecret]';
+    this.getReadmeUrl = 'https://api.github.com/repos/[userName]/[repo]/readme?client_id=[clientId]&client_secret=[clientSecret]';
+
 
   }
-  getUser() {
+  getUser(userName) {
+    this.userName = userName;
     if (this.userName) {
-        return this._http.get('http://api.github.com/users/' + this.userName
-            + '?client_id=' + this.clientId
-            + '&client_secret=' + this.clientSecret);
-            
-            
+      return this._http.get(this.getUserUrl.replace('[userName]', this.userName)
+        .replace('[clientId]', this.clientId)
+        .replace('[clientSecret]', this.clientSecret))
+        .map(res => res.json())
+        .catch(this.handleError);          
       }
   }
-    getRepo() {
-       if (this.userName) {
-          return this._http.get('http://api.github.com/users/' + this.userName
-               + '/repos?client_id=' + this.clientId
-               + '&client_secret=' + this.clientSecret);
+  getRepo(userName) {
+    this.userName = userName;
+    if (this.userName) {
+        return this._http.get(this.getRepoUrl.replace('[userName]', this.userName)
+        .replace('[clientId]', this.clientId)
+        .replace('[clientSecret]', this.clientSecret))
+        .map(res => res.json())
+        .catch(this.handleError);
           }
       
     }
+    getReadme(repoName: string, userName: string) {
+      this.userName = userName;
+      if (this.userName) {
+          return this._http.get(this.getReadmeUrl.replace('[userName]', this.userName)
+              .replace('[clientId]', this.clientId)
+              .replace('[clientSecret]', this.clientSecret)
+              .replace('[repo]', repoName))
+              .map(res => res.json())
+              .catch(this.handleError);
+      }
+  }
     updateUser(userName: string) {
       this.userName = userName;
     }
