6bb52f22691d96750e938328b27d0682ef400e5b
rmautia <raphaelnyangenya@gmail.com>
2020-04-13 17:37:56 +0300

    add getUser.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
 
 
 import { Github } from '../github-class/github';
+import { GithubRequestService } from '../github-http/github-request.service'
 
 @Component({
+  moduleId: module.id,
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  @Input() github: Github;
+  userName: any;
+  user: any;
+  repo: any = [];
+  readmeMdLink: string;
 
-  constructor() { }
+  constructor(private _gitgubRequestService: GithubRequestService, private route: ActivatedRoute, public router: Router) { 
+    this.user = false;
+  }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((v: any) => {
+      this.userName = v.userName;
+    });
+    this._gitgubRequestService.getUser(this.userName).subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+    this._gitgubRequestService.getRepo(this.userName).subscribe(repo => {
+      console.log(repo);
+      this.repo = repo;
+    });
+    
+  }
+  goSearch() {
+    this.router.navigate(['/home-form'])
+  }
+  readmeFun(name: string) {
+    this.router.navigate(['/readme'], { queryParams: { repoName: name, userName: this.userName} });
   }
 
 }
